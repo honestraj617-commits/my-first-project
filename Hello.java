83a3ce6a6099725fc3c44dd6d02faf80517786c8
honestraj617-commits <honestraj617@gmail.com>
2026-01-12 14:04:27 +0530

    Updated Hello.java

--- Hello.java
+++ Hello.java
@@ -1,1 +1,14 @@
-Hello world
+
+
+public class Hello {
+    public static void main(String[] args) {
+        int [] arr = {10,200,30,40,90,80}; 
+        int largest=0;
+        for(int i=1 ; i<arr.length ; i++){
+            if(arr[i]>arr[largest]){
+                largest=i;
+            }
+        }
+        System.err.println(arr[largest]);
+    }
+}
