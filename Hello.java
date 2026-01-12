2487fb0d74446470fa64cbddf54c355ba91b2da5
honestraj617-commits <honestraj617@gmail.com>
2026-01-12 21:24:43 +0530

    got update

--- Hello.java
+++ Hello.java
@@ -1,45 +1,10 @@
 <<<<<<< HEAD
 
-
 public class Hello {
     public static void main(String[] args) {
-        int [] arr = {10,200,30,40,90,80}; 
-        int largest=0;
-        for(int i=1 ; i<arr.length ; i++){
-            if(arr[i]>arr[largest]){
-                largest=i;
-            }
-        }
-        System.err.println(arr[largest]);
-    }
-}
-=======
-
-
-public class CountSentence {
-    public static void main(String[] args) {
-        String str = "Java is Object oriented programming language";
-        boolean isword=false ; 
-        int count=0 ; 
-        for(int i=0;i<str.length();i++){
-            char ch=str.charAt(i) ; 
-            if (ch!=' '){
-            if(!isword){
-                count++;
-                isword=true ; 
-
-            } 
-        }
-        else{
-                isword=false;
-            }
-        
-
-        }
-        
-        System.out.println("Count of the sentence is "+ count);
+       
+        System.out.println("Hello world these my first practice code in Github");
         
         
     }
 }
->>>>>>> 60bc5b58c3b7c4631bcf351c677a765546af156f
